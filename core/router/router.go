6c7bb9ccec6fe480ba314b68904cd5edef35470e
wangxiangbo <wangxiangbo@cmsr.chinamobile.com>
2022-12-23 11:09:16 +0800

    提交statsviz库进行runtime监控

--- core/router/router.go
+++ core/router/router.go
@@ -3,11 +3,21 @@ package router
 import (
 	c "gin-demo/core/controllers"
 	"gin-demo/infra/middlewares"
+	"github.com/arl/statsviz"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter() *gin.Engine {
 	router := gin.New()
+	// 集成statsviz监控
+	router.GET("/debug/statsviz/*filepath", func(context *gin.Context) {
+		if context.Param("filepath") == "/ws" {
+			statsviz.Ws(context.Writer, context.Request)
+			return
+		}
+		statsviz.IndexAtRoot("/debug/statsviz").ServeHTTP(context.Writer, context.Request)
+	})
+
 	// 要在路由组之前全局使用「跨域中间件」, 否则OPTIONS会返回404
 	router.Use(middlewares.Cors(), middlewares.Auth(), middlewares.Recover)
 	timerGroup := router.Group("/api/v1/timers")
