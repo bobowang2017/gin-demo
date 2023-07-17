2ff7c023fe25b1b9a160543ab98a30dd2ed39006
wangxiangbo <wangxiangbo@cmsr.chinamobile.com>
2023-07-17 14:39:27 +0800

    提交base_dao模块代码

--- infra/dao/base_dao.go
+++ infra/dao/base_dao.go
@@ -47,6 +47,13 @@ func (b *BaseDao) GetObjByCondition(model interface{}, params map[string]interfa
 	return temp.Error
 }
 
+func (b *BaseDao) GetOneByCondition(model interface{}, params map[string]interface{}) (err error) {
+	if params == nil {
+		return errors.New("params nil")
+	}
+	return m.DB.Model(model).Offset(0).Limit(1).Where(params).First(model).Error
+}
+
 func (b *BaseDao) DeleteObjById(model interface{}, id int) error {
 	if err := m.DB.Where(map[string]interface{}{"id": id}).Delete(model).Error; err != nil {
 		return err
@@ -89,6 +96,24 @@ func (b *BaseDao) TotalObj(model interface{}, params map[string]interface{}) (in
 	var total int64
 	err := m.DB.Model(model).Where(params).Count(&total).Error
 	return total, err
+}
+
+func (b *BaseDao) TotalPlus(model interface{}, params map[string]interface{}) (int64, error) {
+	var total int64
+	err := m.DB.Model(model).Select("id").Where(params).Count(&total).Error
+	return total, err
+}
+
+func (b *BaseDao) TotalByCondition(model interface{}, params, likeParams map[string]interface{}) (int64, error) {
+	var (
+		temp  = m.DB.Model(model).Where(params)
+		total int64
+	)
+	for k, v := range likeParams {
+		temp = temp.Where(k+" like ?", "%"+v.(string)+"%")
+	}
+	temp = temp.Count(&total)
+	return total, temp.Error
 }
 
 func (b *BaseDao) ExecSql(sql string, values ...interface{}) (*gorm.DB, error) {
